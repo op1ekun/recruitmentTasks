804b117d135ea8a8e1dd44b8d54336264acb9cb4
op1ekun <op1ekun@borntoco.de>
2016-04-04 00:50:31 +0100

    Some very basic UI

--- wipro_digital/app/app.js
+++ wipro_digital/app/app.js
@@ -1,6 +1,24 @@
 import {getForecast} from 'app/forecast/forecast.services.js';
 
-getForecast()
+const IMPERIAL = 'imperial';
+const METRIC = 'metric';
+let units = IMPERIAL;
+
+getForecast(undefined, undefined, units)
     .then(function(forecastData) {
+        'use strict';
+
+        const listLength = forecastData.list.length;
+
+        const cityCnt = document.querySelector('.city');
+        const rainCnt = document.querySelector('.rain > span');
+        const tempCnt = document.querySelector('.temp > span');
+        const presCnt = document.querySelector('.pres > span');
+
+        cityCnt.innerHTML = forecastData.city.name;
+        rainCnt.innerHTML = (forecastData.list[listLength-1].rain['3h'] || 0) * 100 + 'mm';
+        tempCnt.innerHTML = Math.round(forecastData.list[listLength-1].main.temp) + (units === METRIC ? '&deg;C' : '&deg;F');
+        presCnt.innerHTML = Math.round(forecastData.list[listLength-1].main.pressure) + 'hPa';
+
         console.log(forecastData);
     });
--- wipro_digital/app/forecast/forecast.services.js
+++ wipro_digital/app/forecast/forecast.services.js
@@ -1,22 +1,39 @@
 import config from  '../../config.js';
 
-function queryURLTmpl(city, country) {
+/**
+ * [queryURLTmpl description]
+ * @param  {[type]} city    [description]
+ * @param  {[type]} country [description]
+ * @param  {[type]} units   [description]
+ * @return {[type]}         [description]
+ */
+function queryURLTmpl(city, country, units) {
+    'use strict';
+
     const apiKey = config.apiKey;
 
-    return `http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&mode=json&appid=${apiKey}`;
+    return `http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&mode=json&appid=${apiKey}&units=${units}`;
 }
 
-export function getForecast(city = 'London', country= 'UK') {
+/**
+ * [getForecast description]
+ * @param  {String} city    [description]
+ * @param  {String} country [description]
+ * @param  {String} units   [description]
+ * @return {[type]}         [description]
+ */
+export function getForecast(city = 'London', country = 'UK', units = 'imperial') {
+    'use strict';
 
-    const queryURL = queryURLTmpl(city, country);
+    const queryURL = queryURLTmpl(city, country, units);
 
     return fetch(queryURL, {
-        method: 'get'
-    })
-    .then(function(response) {
-        return response.json();
-    })
-    .catch(function(err) {
-        console.error(err);
-    });
+            method: 'get'
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
 }
